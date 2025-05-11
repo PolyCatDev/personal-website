caa89f9347fdc79acf3f988b47e7e607c9b8f93d
PolyCat <contact.polycat@proton.me>
2025-05-11 14:03:00 +0300

    removed astro-icon package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-import icon from 'astro-icon';
-
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [icon({
-      iconDir: "src/assets/icons",
-    })],
 });
