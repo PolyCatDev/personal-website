0ca32da012fe7f9416858faf82211bd3a577a590
PolyCat <contact.polycat@proton.me>
2026-03-13 11:25:11 +0200

    Reset website back to a blank project This is the start of the rewrite to my personal website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-});
+// https://astro.build/config
+export default defineConfig({});
